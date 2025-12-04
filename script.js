fcf5801d7cf9196e6574fbd118bc71773fd82ed3
Jaeden Cox Arnsdorf <jaeden@uw.edu>
2025-12-04 10:27:42 -0800

    Added custom map styling to map

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const API_KEY = "Ioaw398mdAchulbDmmlaESbNv7PekHPPm87Mk1eG";
 
+<<<<<<< HEAD
 const map = L.map("map").setView([47.6062, -122.3321], 10);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -13,6 +14,13 @@ const evIcon = L.icon({
   iconSize: [35, 35],
   iconAnchor: [17, 35],
   popupAnchor: [0, -30],
+=======
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/jaedencca/cmirrmnb5002501sn6k914u71",
+  center: [-122.3321, 47.6062],
+  zoom: 10,
+>>>>>>> 5fbf570 (Added custom map styling to map)
 });
 
 async function loadStations() {
